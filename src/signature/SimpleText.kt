aad8e1886bd5d6155f6df2bfb249fca7bcb80edf
Safi <safiullahkakar2020@outlook.com>
2024-09-15 20:41:14 +0430

    Added stage 1 solution code

---
+++ src/signature/SimpleText.kt
@@ -0,0 +1,10 @@
+package signature
+
+fun main(args: Array<String>) {
+    val str = buildString {
+        appendLine(" _____________")
+        appendLine(" | Hyper Skill |")
+        appendLine(" •••••••••••••")
+    }
+    println(str)
+}
